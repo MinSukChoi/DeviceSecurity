26f41a1f1ddd7426105a7e326d03153d26b61d18
Keunsol <keunsol8247@naver.com>
2015-10-15 11:06:50 +0900

    * blinder added

--- StudyBrowser/app/src/main/java/soma/devicesecurity/studybrowser/MainActivity.java
+++ StudyBrowser/app/src/main/java/soma/devicesecurity/studybrowser/MainActivity.java
@@ -1,19 +1,27 @@
 package soma.devicesecurity.studybrowser;
 
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
     @Bind(R.id.webView) WebView webView;
     @Bind(R.id.editText_address) EditText editText_address;
+    @Bind(R.id.imageView_block) ImageView imageView_block;
     @OnClick(R.id.button_move)
     public void move() {
         String url = editText_address.getText().toString();
@@ -34,9 +42,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class WebClient extends WebViewClient {
+        private int delay = 1000;
+        private boolean isRedirected;
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
+            isRedirected = true;
             return true;
         }
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+
+            if (!isRedirected) {
+                imageView_block.setVisibility(View.VISIBLE);
+                Log.v("TEST", "HEHE");
+            }
+
+            isRedirected = false;
+        }
+        public void onPageFinished(WebView view, String url) {
+            if(!isRedirected) {
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageView_block.setVisibility(View.INVISIBLE);
+                    }
+                }, 1000);
+            delay *= 2;
+            Log.v("TEST", ""+delay);
+            }
+        }
+
     }
 }
