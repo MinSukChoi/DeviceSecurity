10b93e0e79705f2bb2b4bcb7801913bc845393fe
EUNJIN Aws1 <magopark18@gmail.com>
2015-10-28 15:18:39 +0900

    no message

---
+++ MyApplication/app/src/main/java/com/example/park/myapplication/AppDetail.java
@@ -0,0 +1,12 @@
+package com.example.park.myapplication;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by PARK on 15. 10. 10..
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
